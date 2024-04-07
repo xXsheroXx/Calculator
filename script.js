7c967359d90a34e3d75e0bbbe5856a8eb702d9b7
xXsheroXx <shero010807@gmail.com>
2024-04-08 00:40:04 +0200

    edited getDisplayNumber() function

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ class Calculator {
         this.previousOperand = '';
         this.currentOperand = '';
         this.operation = undefined;
+        console.log('Cleared!');
     }
 
     delete() {
@@ -59,15 +60,28 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        const floatNumber = parseFloat(number);
-        if(isNaN(floatNumber)) return '';
-        return floatNumber.toLocaleString('en');
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const  decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if(isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits : 0});
+        }
+        if(decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        }else {
+            return integerDisplay;
+        }
     }
 
     displayUpdates() {
         this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
         if(this.operation != null) {
             this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        }else {
+            this.previousOperandTextElement.innerText = '';
         }
     }
 }
